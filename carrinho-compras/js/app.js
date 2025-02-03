3d377173af4361c89c3f35920b68508d8bed4d2e
Caio Ribeiro <caiomgr@hotmail.com>
2025-02-03 19:45:06 +0000

    populando codigo JS

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,25 @@
+let totalGeral;
+limpar();
+
+function adicionar() {
+    let produto = document.getElementById("produto").value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('$')[1];
+    let quantidade = document.getElementById("quantidade").value;
+    let valorTotalProduto = quantidade * valorUnitario;
+    let listaProdutos = document.getElementById('lista-produtos');
+    listaProdutos.innerHTML = listaProdutos.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${valorTotalProduto}</span>
+        </section>`; 
+    totalGeral = totalGeral + valorTotalProduto;    
+    let totalCarrinho = document.getElementById('valor-total');
+    totalCarrinho.textContent = `R$ ${totalGeral}`;    
+    document.getElementById("quantidade").value = 0;
+    
+}
+
+function limpar() {
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
+    totalGeral = 0;
+}
